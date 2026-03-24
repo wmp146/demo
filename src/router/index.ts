21170ef37c0440d28c3be37014cb438fca1467ef
WMPSSG <8479564+wmpssg@user.noreply.gitee.com>
2026-03-24 15:03:05 +0800

    feat: 初始化基本内容

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import type { App } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
 
+const routerMap: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: '/home', // 添加这行实现自动跳转
+    meta: { title: '首页' },
+    children: [
+      {
+        name: 'Home',
+        path: '/home',
+        component: () => import('@/App.vue'),
+      },
+    ],
+  },
+  // {
+  //     path: '/login',
+  //     name: 'Login',
+  //     component: () => import('@/views/login/index.vue'),
+  //     meta: { title: '登录页' },
+  // }
+]
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  history: createWebHashHistory(import.meta.env.VITE_BASE_PATH),
+  strict: true,
+  routes: [...routerMap],
 })
+
+export const setupRouter = (app: App<Element>) => {
+  app.use(router)
+}
 
 export default router
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,67 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { fileURLToPath } from 'node:url'
+import { defineConfig, loadEnv, ConfigEnv, UserConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import UnoCss from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import path from 'node:path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+  const env = loadEnv(mode, process.cwd())
+  console.log('当前环境变量：', env)
+  return {
+    plugins: [
+      vue(),
+      vueJsx(),
+      vueDevTools(),
+      UnoCss({ configFile: './uno.config.ts' /*注意这里的后缀名*/ }),
+      AutoImport({
+        // 自动生成类型声明文件，方便 TypeScript 智能提示。
+        dts: 'src/auto-imports.d.ts',
+        // resolvers: [ElementPlusResolver()]
+        // 自动按需导入 Element Plus 组件库相关的 API。
+        resolvers: [ElementPlusResolver()],
+        // [imports: [{ 'element-plus/es': ['ElMessageBox', 'ElMessage', 'ElNotification'] }]](http://vscodecontentref/2)
+        // 自动导入 element-plus/es 包下的 ElMessageBox、ElMessage、ElNotification，你在代码里直接用这些 API，无需手动 import。
+        imports: [{ 'element-plus/es': ['ElMessageBox', 'ElMessage', 'ElNotification'] }],
+      }),
+    ],
+    resolve: {
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.scss', '.css'],
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@components': path.resolve(__dirname, './src/components'),
+        '@utils': path.resolve(__dirname, './src/utils'),
+      },
     },
-  },
+    build: {
+      minify: 'terser',
+      outDir: env.VITE_OUT_DIR || 'dist',
+      // 通过环境变量控制是否输出内联 sourcemap。
+      sourcemap: env.VITE_SOURCEMAP === 'true' ? 'inline' : false,
+      reportCompressedSize: false,
+      terserOptions: {
+        compress: {
+          drop_debugger: env.VITE_DROP_DEBUGGER === 'true',
+          drop_console: env.VITE_DROP_CONSOLE === 'true',
+        },
+      },
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vue: ['vue'],
+            'element-plus': ['element-plus'],
+            'lodash-es': ['lodash-es'],
+            axios: ['axios'],
+            'vue-router': ['vue-router'],
+            pinia: ['pinia', 'pinia-plugin-persistedstate'],
+          },
+        },
+      },
+    },
+  }
 })
